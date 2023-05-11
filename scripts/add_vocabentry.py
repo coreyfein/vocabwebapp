89cb3be3a88d41a0413d8826af0f3fb6f12758f3
Corey Fein <coreyfein@gmail.com>
2023-05-11 02:16:39 -0400

    got it working with merriam-webster api, so now creating a vocab entry actually gets real word data

--- scripts/add_vocabentry.py
+++ scripts/add_vocabentry.py
@@ -2,17 +2,20 @@ from simplevocab.models import Word, VocabEntry
 import sys
 import json
 import requests
+import re
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 def run(word_input, discovery_source_input, user):    
-    lemma = get_lemma_for_word(word_input)
-    w, created = Word.objects.get_or_create(word__iexact=lemma)
+    # lemma = get_lemma_for_word(word_input) #OED version only
+    # w, created = Word.objects.get_or_create(word__iexact=lemma) #OED version only
+    w, created = Word.objects.get_or_create(word__iexact=word_input)
     print("word id: " + str(w.id))
     print("Created: {}".format(created))
     if created:
-        word, definition_string, synonyms_string, examples_string, etymology = get_dictionary_data(lemma)
+        # word, definition_string, synonyms_string, examples_string, etymology = get_dictionary_data(lemma) #OED version
+        word, definition_string, synonyms_string, examples_string, etymology = get_webster_dictionary_data(word_input)
         # when triggered by a form, also pass in user to the created_by field in new Word record
         w.created_by = user
         w.word = word
@@ -114,6 +117,62 @@ def get_dictionary_data(word):
 
     dictionary_data = (word, definition_string, synonyms_string, examples_string, etymology)
     return dictionary_data
+
+def get_webster_dictionary_data(word):
+    MERRIAM_WEBSTER_DICTIONARY_KEY = os.environ["MERRIAM_WEBSTER_DICTIONARY_KEY"]
+    url = "https://dictionaryapi.com/api/v3/references/collegiate/json/{}?key={}".format(word, MERRIAM_WEBSTER_DICTIONARY_KEY)
+    response = requests.get(url)
+    response_content_str = response.content.decode()
+    response_content_list = json.loads(response_content_str)
+    print(json.dumps(response_content_list, indent=4))
+    word_dict = response_content_list[0]
+    word = word_dict["meta"]["id"].split(":")[0]
+    part_of_speech = word_dict["fl"]
+    senses = word_dict["def"][0]["sseq"]
+    definition = ""
+    print(len(senses))
+    for count, sense in enumerate(senses):
+        if definition != "":
+            print(len(definition))
+            continue
+        else:
+            print("trying sense {}".format(count))
+        sense_dict = sense[0][1]
+        definition_list = sense_dict["dt"]
+        synonyms_string = ""
+        examples_string = ""
+        etymology = ""
+        for component in definition_list:
+            if component[0] == "text":
+                definition = re.sub(r'{.+?}', '', component[1])# the regex replaces all text between consecutive { and } (and the braces themselves) with an empty string
+            if component[0] == "vis":
+                examples = []
+                for example_dict in component[1]:
+                    example = re.sub(r'{.+?}', '', example_dict["t"])# the regex replaces all text between consecutive { and } (and the braces themselves) with an empty string
+                    examples.append(example)
+                examples_string = "; ".join(examples)
+    definition_string = '({}) {}'.format(part_of_speech, definition)
+    syns = word_dict.get("syns")
+    if not syns:
+        # print("No synonyms found")
+        pass
+    else:
+        synonyms_list = syns[0]["pt"][0]
+    
+        for count, component in enumerate(synonyms_list):
+            if component == "text":
+                synonyms_string = synonyms_list[count + 1].replace("{/sc} {sc}", ", ").replace("{/sc}", "")
+                synonyms_string = re.sub(r'{.+?}', '', synonyms_string)# the regex replaces all text between consecutive { and } (and the braces themselves) with an empty string
+    
+    etymology_list = word_dict.get("et")
+    for component in etymology_list:
+        if component[0] == "text":
+            etymology = re.sub(r'{.+?}', '', component[1])# the regex replaces all text between consecutive { and } (and the braces themselves) with an empty string
+    dictionary_data = (word, definition_string, synonyms_string, examples_string, etymology)
+    print(dictionary_data)
+    return dictionary_data
+    
+    
 
 if __name__ == "__main__":
     run()
