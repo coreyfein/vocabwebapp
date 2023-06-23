2de4148880c5163bbce707d2bd03e6a82deed0dd
Corey Fein <coreyfein@gmail.com>
2023-06-23 00:36:27 -0400

    removed /words urls

--- simplevocab/urls.py
+++ simplevocab/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 app_name='simplevocab'
 urlpatterns = [
-    path('words', views.WordListView.as_view(), name='all_words'),
-    path('word/<int:pk>', views.WordDetailView.as_view(), name='word_detail'),
-    path('word/create',
-        views.WordCreateView.as_view(success_url=reverse_lazy('simplevocab:all_words')), name='word_create'),
-    path('word/<int:pk>/update',
-        views.WordUpdateView.as_view(success_url=reverse_lazy('simplevocab:all_words')), name='word_update'),
-    path('word/<int:pk>/delete',
-        views.WordDeleteView.as_view(success_url=reverse_lazy('simplevocab:all_words')), name='word_delete'),
+    # path('words', views.WordListView.as_view(), name='all_words'),
+    # path('word/<int:pk>', views.WordDetailView.as_view(), name='word_detail'),
+    # path('word/create',
+    #     views.WordCreateView.as_view(success_url=reverse_lazy('simplevocab:all_words')), name='word_create'),
+    # path('word/<int:pk>/update',
+    #     views.WordUpdateView.as_view(success_url=reverse_lazy('simplevocab:all_words')), name='word_update'),
+    # path('word/<int:pk>/delete',
+    #     views.WordDeleteView.as_view(success_url=reverse_lazy('simplevocab:all_words')), name='word_delete'),
     path('vocabentries', views.VocabEntryListView.as_view(), name='all_vocabentries'),
     path('vocabentry/<int:pk>/update',
         views.VocabEntryUpdateView.as_view(success_url=reverse_lazy('simplevocab:all_vocabentries')), name='vocabentry_update'),
