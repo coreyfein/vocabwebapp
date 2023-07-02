3bb329546349b06bf3ba7af2084e6ff4df3f4037
Corey Fein <coreyfein@gmail.com>
2023-07-01 23:32:10 -0400

    added hamburger for mobile menu; added container divs; deleted word model templates, urls, and views

--- simplevocab/urls.py
+++ simplevocab/urls.py
@@ -3,19 +3,11 @@ from . import views
 
 app_name='simplevocab'
 urlpatterns = [
-    # path('words', views.WordListView.as_view(), name='all_words'),
-    # path('word/<int:pk>', views.WordDetailView.as_view(), name='word_detail'),
-    # path('word/create',
-    #     views.WordCreateView.as_view(success_url=reverse_lazy('simplevocab:all_words')), name='word_create'),
-    # path('word/<int:pk>/update',
-    #     views.WordUpdateView.as_view(success_url=reverse_lazy('simplevocab:all_words')), name='word_update'),
-    # path('word/<int:pk>/delete',
-    #     views.WordDeleteView.as_view(success_url=reverse_lazy('simplevocab:all_words')), name='word_delete'),
     path('vocabentries', views.VocabEntryListView.as_view(), name='all_vocabentries'),
     path('vocabentry/<int:pk>',
         views.VocabEntryDetailView.as_view(), name='vocabentry_detail'),
     path('vocabentry/<int:pk>/update',
-        views.VocabEntryUpdateView.as_view(), name='vocabentry_update'),#success_url set in view to vocabentry_detail for the pk
+        views.VocabEntryUpdateView.as_view(), name='vocabentry_update'),#success_url set in view to vocabentry_detail for the pk in the url
     path('vocabentry/<int:pk>/delete',
         views.VocabEntryDeleteView.as_view(success_url=reverse_lazy('simplevocab:all_vocabentries')), name='vocabentry_delete'),
     path('vocabentry/create',
--- simplevocab/views.py
+++ simplevocab/views.py
@@ -10,33 +10,6 @@ from scripts import add_vocabentry, create_quiz_queue, add_quizresponses, get_re
 import csv
 from io import TextIOWrapper
 
-class WordListView(OwnerListView):
-    model = Word
-    field_to_filter_by = "created_by"
-    # By convention:
-    # template_name = "simplevocab/words_list.html"
-
-class WordDetailView(OwnerDetailView):
-    model = Word
-
-# WordCreateView(OwnerUpdateView) creates a word in the Word table with manually entered word data. 
-# Instead, a new version should just receive a form input with word and discovery_source, 
-# get_or_create a Word record, update the Word record with definitions etc. if just created,
-# and create a VocabEntry record (user, discovery_source, word_id)
-# Standalone script add_vocabentry basically does this, needs to be adapated to a view
-class WordCreateView(OwnerCreateView):
-    model = Word
-    # List the fields to copy from the Words model to the Word form
-    fields = ['word', 'definition', 'synonyms', 'examples', 'etymology']
-
-class WordUpdateView(OwnerUpdateView):
-    model = Word
-    field_to_filter_by = "created_by"
-    fields = ['word', 'definition', 'synonyms', 'examples', 'etymology']
-
-class WordDeleteView(OwnerDeleteView):
-    model = Word
-
 class VocabEntryListView(OwnerListView):
     model = VocabEntry
     field_to_filter_by = "user"
