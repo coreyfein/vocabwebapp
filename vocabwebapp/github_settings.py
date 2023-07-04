5075ccacc23462b1d108d210da76ce8d084bf0b0
Corey Fein <coreyfein@gmail.com>
2023-07-03 23:00:47 -0400

    cleanup comments

--- vocabwebapp/github_settings.py
+++ vocabwebapp/github_settings.py
@@ -8,13 +8,6 @@ load_dotenv()
 
 # Add a New OAuth2 App
 
-# Using PythonAnywhere here are some settings:
-
-# Application name: ChuckList PythonAnywhere
-# Homepage Url: https://drchuck.pythonanywhere.com
-# Application Description: Whatever
-# Authorization callback URL: https://drchuck.pythonanywhere.com/oauth/complete/github/
-
 # Also on PythonAnywhere, go into the Web tab and enable "Force HTTPS"
 # so you don't get a redirect URI mismatch.
 
@@ -32,9 +25,3 @@ SOCIAL_AUTH_GITHUB_SCOPE = ['user:email']
 
 # Using ngrok is hard because the url changes every time you start ngrok
 
-# If you are running on localhost, here are some settings:
-
-# Application name: ChuckList Local
-# Homepage Url: http://localhost:8000
-# Application Description: Whatever
-# Authorization callback URL: http://127.0.0.1:8000/oauth/complete/github/
